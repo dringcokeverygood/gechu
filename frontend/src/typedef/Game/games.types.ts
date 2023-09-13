926285449551c3af784e2ddb645f871866a263e8
심은진 <seg412@naver.com>
2023-09-13 15:44:10 +0900

    Feat: Common에 game card 컴포넌트 생성

---
+++ frontend/src/typedef/Game/games.types.ts
@@ -0,0 +1,11 @@
+export type GamePreviewType = {
+	gameSeq: number;
+	gameTitle: string;
+	gameTitleImageUrl: string;
+	develop: string;
+	publish: string;
+	createDate: string;
+	estimatePercent: number;
+	genre: string;
+	platform: string;
+};
