9a796664ad0458c8e4cdb0090dafd5e957703210
신경희 <skh1615@gmail.com>
2023-09-14 15:13:29 +0900

    게임탭 리다이렉트

--- frontend/src/routes/RootRouter.tsx
+++ frontend/src/routes/RootRouter.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import LayoutWithHeader from '../pages/LayoutWithHeader';
 import Home from '../pages/Home';
 import Game from '../pages/Game';
 import GameReviewContainer from '../components/Games/containers/GameReviewContainer';
+import GameArticleListContainer from '../components/Games/containers/GameArticleListContainer';
 
 const RootRouter = () => {
 	return (
@@ -13,8 +14,9 @@ const RootRouter = () => {
 				<Route path="/" element={<LayoutWithHeader />}>
 					<Route index element={<Home />} />
 					<Route path="/game-detail" element={<Game />}>
-						<Route index element={<GameReviewContainer/>}/>
-						{/* <Route path="articles" element={}/> */}
+						<Route path="" element={<Navigate replace to="reviews" />} />
+						<Route path="reviews" element={<GameReviewContainer />} />
+						<Route path="articles" element={<GameArticleListContainer />} />
 					</Route>
 				</Route>
 			</Routes>
