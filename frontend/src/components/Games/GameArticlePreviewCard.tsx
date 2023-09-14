fdd0508f28787d069ca6ac48fc48e494b3316ac6
신경희 <skh1615@gmail.com>
2023-09-14 10:44:01 +0900

    Feat: 게시글미리보기 작성자프로필띄우기

--- frontend/src/components/Games/GameArticlePreviewCard.tsx
+++ frontend/src/components/Games/GameArticlePreviewCard.tsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import { GameArticlePreviewType } from './containers/GameArticleListContainer'
+import React from 'react';
+import { GameArticlePreviewType } from './containers/GameArticleListContainer';
 
-const GameArticlePreviewCard = ({article}: {article: GameArticlePreviewType}) => {
-  return (
-    <div className="flex flex-col h-[280px] text-white-200 bg-white-900 rounded-md py-2 px-2 space-y-2 mb-4">
-      <img src={article.imageUrl} alt="article-thumbnail" 
-        className="h-2/3 w-fit self-center"/>
-        {/* <div className="flex justify-between w-full items-center">
-            <div className="flex flex-row items-center space-x-4">
-                <img
-                className="inline-block h-10 w-10 rounded-full ring-2 ring-red-600"
-                src="https://images.unsplash.com/photo-1552944150-6dd1180e5999?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1925&q=80"
-                alt=""
-                />
-                <div className="font-dungGeunMo text-xl">{article.userNickname}</div>
-            </div>
-        </div> */}
-        <div className="font-dungGeunMo text-xl">
-            {article.content}
-        </div>
-    </div>
-  )
-}
+const GameArticlePreviewCard = ({
+	article,
+}: {
+	article: GameArticlePreviewType;
+}) => {
+	return (
+		<div className="mb-4 flex h-[280px] flex-col space-y-2 rounded-md bg-white-900 px-2 py-2 text-white-200">
+			<div className="flex flex-row items-center space-x-4">
+				<img
+					className="inline-block h-10 w-10 rounded-full ring-2 ring-red-600"
+					src="https://images.unsplash.com/photo-1552944150-6dd1180e5999?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1925&q=80"
+					alt=""
+				/>
+				<div className="text-md font-dungGeunMo">{article.userNickname}</div>
+			</div>
+			<img
+				src={article.imageUrl}
+				alt="article-thumbnail"
+				className="h-2/3 w-fit self-center"
+			/>
+			<div className="font-dungGeunMo text-xl">{article.articleTitle}</div>
+		</div>
+	);
+};
 
-export default GameArticlePreviewCard
+export default GameArticlePreviewCard;
