9af7bf30296c6df3c1e6476265d2ccc055b05908
심은진 <seg412@naver.com>
2023-09-14 15:16:23 +0900

    필터 사이드바 컴포넌트 생성 중

---
+++ frontend/src/typedef/Game/checkbox.types.ts
@@ -0,0 +1,18 @@
+export type CheckboxType = {
+	id: string;
+	text: string;
+	checked: boolean;
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export interface FilterObject {
+	[key: string]: {
+		text: string;
+		flag: boolean;
+	};
+}
+
+export type FilterType = {
+	filterState: FilterObject;
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
