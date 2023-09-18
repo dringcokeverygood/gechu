034cf83e7ca19e6803eb96d3a999aea2eff47944
이찬희 <ych0716@naver.com>
2023-09-18 12:45:00 +0900

    setting: build.gradle 설정 부족하던거 추가
    크롤링 서버 설정파일

--- crawl/src/main/java/com/gechu/crawl/CrawlApplication.java
+++ crawl/src/main/java/com/gechu/crawl/CrawlApplication.java
@@ -2,8 +2,10 @@ package com.gechu.crawl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CrawlApplication {
 
 	public static void main(String[] args) {
