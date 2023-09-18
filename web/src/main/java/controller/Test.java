b62dabe922ded476ac632ed92ea8bd463a7764f1
이찬희 <ych0716@naver.com>
2023-09-18 14:05:10 +0900

    feat: api gateway 테스트용 컨트롤러 재작성

--- web/src/main/java/controller/Test.java
+++ web/src/main/java/controller/Test.java
@@ -1,13 +1,14 @@
 package controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Test {
 
-	@GetMapping("/test")
-	public String test() {
-		return "test";
+	@GetMapping("/{path}")
+	public String test(@PathVariable String path) {
+		return path;
 	}
 }
