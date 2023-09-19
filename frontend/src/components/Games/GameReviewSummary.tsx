5fc1a1a66e271e8ffcb86cc7ef306c1e8ede2db0
신경희 <skh1615@gmail.com>
2023-09-19 17:57:33 +0900

    Feat: chart.js 좋아요싫어요 그래프

---
+++ frontend/src/components/Games/GameReviewSummary.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+// import type { ChartData, ChartOptions } from 'chart.js';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { CategoryScale, LinearScale, BarElement, Chart } from 'chart.js';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Bar } from 'react-chartjs-2';
+import { MdThumbUp, MdThumbDown } from 'react-icons/md';
+
+Chart.register(CategoryScale, LinearScale, BarElement);
+
+interface GameReviewSummaryProps {
+	reviewCnt: number;
+	likeCnt: number;
+	dislikeCnt: number;
+}
+// interface BarProps {
+// 	options: ChartOptions<'bar'>;
+// 	data: ChartData<'bar'>;
+// }
+
+const GameReviewSummary: React.FC<GameReviewSummaryProps> = ({
+	reviewCnt,
+	likeCnt,
+	dislikeCnt,
+}) => {
+	const likeRate = ((100 * likeCnt) / (likeCnt + dislikeCnt)).toFixed(2);
+	const dislikeRate = ((100 * dislikeCnt) / (likeCnt + dislikeCnt)).toFixed(2);
+	const options = {
+		indexAxis: 'y' as const,
+		plugins: {
+			tooltip: {
+				enabled: true,
+				mode: 'index' as const,
+			},
+		},
+		responsive: true,
+		aspectRatio: 12,
+		layout: {
+			padding: 0,
+		},
+		scales: {
+			x: {
+				stacked: true,
+				display: false,
+			},
+			y: {
+				stacked: true,
+				display: false,
+			},
+		},
+	};
+	const data = {
+		labels: [''],
+		datasets: [
+			{
+				label: '좋아요',
+				backgroundColor: '#7390ff',
+				data: [likeCnt],
+				borderRadius: 16,
+				borderSkipped: 'middle' as const,
+			},
+			{
+				label: '싫어요',
+				backgroundColor: '#ff5964',
+				data: [dislikeCnt],
+				borderRadius: 16,
+				borderSkipped: 'middle' as const,
+			},
+		],
+	};
+	return (
+		<div className="flex flex-col py-2 text-white-200">
+			<div className="flex flex-row justify-between font-dungGeunMo text-lg">
+				<div>{reviewCnt}건</div>
+				<div>작성하기</div>
+			</div>
+			<div className="flex h-32 flex-row items-center justify-center space-x-2">
+				<div className="flex flex-row items-center justify-between space-x-2 font-dungGeunMo text-xl text-blue-400">
+					<MdThumbUp />
+					{likeRate}%
+				</div>
+				<div className="flex w-full flex-row items-center">
+					<Bar options={options} data={data} />
+				</div>
+				<div className="flex flex-row-reverse items-center font-dungGeunMo  text-xl text-red-400">
+					<MdThumbDown />
+					{dislikeRate}%
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default GameReviewSummary;
