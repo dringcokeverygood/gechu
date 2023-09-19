62861d399f2bde100c1c338a52c85806605145b2
백승은 <tmdms12@gmail.com>
2023-09-19 11:19:22 +0900

    feat: 뉴스 검색결과 카드 추가
    - 뉴스 카드 추가

--- frontend/src/components/Search/SearchNews.tsx
+++ frontend/src/components/Search/SearchNews.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect } from 'react';
+import BNewsCardContainer from '../Common/containers/BNewsCardContainer';
+import { GameNewsPreviewType } from '../../typedef/main.types';
 
-const SearchNews = () => {
+type NewsCardProps = {
+	newsList: GameNewsPreviewType[];
+};
+
+const SearchNews = ({ newsList }: NewsCardProps) => {
 	useEffect(() => {
 		return () => console.log('뉴스 언마운트');
 	}, []);
@@ -9,6 +15,14 @@ const SearchNews = () => {
 		<div>
 			<p>뉴스 검색 api 호출 </p>
 			<p>검색 결과</p>
+
+			<div className="flex w-[1200px] flex-col justify-center">
+				<div className="flex flex-wrap content-start items-start justify-start gap-6 self-stretch pb-10 ">
+					{newsList.map((news) => (
+						<BNewsCardContainer key={news.news_seq} news={news} />
+					))}
+				</div>
+			</div>
 		</div>
 	);
 };
