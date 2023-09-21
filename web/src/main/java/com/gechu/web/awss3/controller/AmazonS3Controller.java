f0fd4dcb68c622f16f262ce170d9593ba07f2494
장성운 <sungoon0646@gmail.com>
2023-09-21 10:22:56 +0900

    feat : web-backend에 s3 적용

---
+++ web/src/main/java/com/gechu/web/awss3/controller/AmazonS3Controller.java
@@ -0,0 +1,38 @@
+package com.gechu.web.awss3.controller;
+
+import com.gechu.web.awss3.service.AwsS3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/s3")
+public class AmazonS3Controller {
+
+    private final AwsS3Service awsS3Service;
+
+    /**
+     * Amazon S3에 파일 업로드
+     * @return 성공 시 200 Success와 함께 업로드 된 파일의 파일명 리스트 반환
+     */
+    @PostMapping("/file")
+    public ResponseEntity<List<String>> uploadFile(@RequestPart List<MultipartFile> multipartFile) {
+        List<String> uploadedFiles = awsS3Service.uploadFile(multipartFile);
+        return new ResponseEntity<>(uploadedFiles, HttpStatus.OK);
+    }
+
+    /**
+     * Amazon S3에 업로드 된 파일을 삭제
+     * @return 성공 시 200 Success
+     */
+    @DeleteMapping("/file")
+    public ResponseEntity<Void> deleteFile(@RequestParam String fileName) {
+        awsS3Service.deleteFile(fileName);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+}
