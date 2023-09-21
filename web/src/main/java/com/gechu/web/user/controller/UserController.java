839c9f0f94fc98ff8e99a8d450a6a83a5b515d66
장성운 <sungoon0646@gmail.com>
2023-09-21 17:44:09 +0900

    feat : regacy한 블로그의 로그인 구현(폐기예정)

--- web/src/main/java/com/gechu/web/user/controller/UserController.java
+++ web/src/main/java/com/gechu/web/user/controller/UserController.java
@@ -3,26 +3,29 @@ package com.gechu.web.user.controller;
 import com.gechu.web.user.service.UserService;
 import com.gechu.web.user.util.JwtToken;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/user")
 public class UserController {
 
-    private final UserService service;
+    private final UserService userService;
 
-    public UserController(UserService service) {
-        this.service = service;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping("/login")
     public ResponseEntity<JwtToken> loginSuccess(@RequestBody Map<String, String> loginForm) {
-        JwtToken token = service.login(loginForm.get("username"), loginForm.get("password"));
+        JwtToken token = userService.login(loginForm.get("username"), loginForm.get("password"));
         return ResponseEntity.ok(token);
+    }
+
+    @GetMapping("/auth/{provider}")
+    public ResponseEntity<LoginResponse> login(@PathVariable String provider, @RequestParam String code) {
+        LoginResponse loginResponse = userSerivce.login(provider, code);
+
+        return ResponseEntity.ok().body(loginResponse);
     }
 }
--- web/src/main/java/com/gechu/web/user/service/UserService.java
+++ web/src/main/java/com/gechu/web/user/service/UserService.java
@@ -3,15 +3,31 @@ package com.gechu.web.user.service;
 import com.gechu.web.user.repository.UserRepository;
 import com.gechu.web.user.util.JwtToken;
 import com.gechu.web.user.util.JwtTokenProvider;
+import com.nimbusds.openid.connect.sdk.claims.Gender;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Map;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
     private final BCryptPasswordEncoder encoder;
@@ -19,12 +35,10 @@ public class UserService {
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final JwtTokenProvider jwtTokenProvider;
 
-    public UserService(BCryptPasswordEncoder encoder, UserRepository repository, AuthenticationManagerBuilder authenticationManagerBuilder, JwtTokenProvider jwtTokenProvider) {
-        this.encoder = encoder;
-        this.repository = repository;
-        this.authenticationManagerBuilder = authenticationManagerBuilder;
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
+    private static final String BEARER_TYPE = "Bearer";
+    private final InMemoryClientRegistrationRepository inMemoryRepository;
+    private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public JwtToken login(String email, String password) {
         // Authentication 객체 생성
@@ -35,5 +49,74 @@ public class UserService {
         JwtToken token = jwtTokenProvider.generateToken(authentication);
 
         return token;
+    }
+
+    public LoginResponse loginSuccess(String providerName, String code) {
+        ClientRegistration provider = inMemoryRepository.findByRegistrationId(providerName);
+        OauthTokenResponse tokenResponse = getToken(code, provider);
+        User user = getUserProfile(providerName, tokenResponse, provider);
+
+        String accessToken = jwtTokenProvider.createAccessToken(String.valueOf(user.getId()));
+        String refreshToken = jwtTokenProvider.createRefreshToken();
+
+        return LoginResponse.builder()
+                .id(user.getId())
+                .name(user.getUserProfile().getNickName())
+                .email(user.getEmail())
+                .imageUrl(user.getUserProfile().getImageUrl())
+                .role(user.getRole())
+                .tokenType(BEARER_TYPE)
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    private OauthTokenResponse getToken(String code, ClientRegistration provider) {
+        return WebClient.create()
+                .post()
+                .uri(provider.getProviderDetails().getTokenUri())
+                .headers(header -> {
+                    header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+                    header.setAcceptCharset(Collections.singletonList(StandardCharsets.UTF_8));
+                })
+                .bodyValue(tokenRequest(code, provider))
+                .retrieve()
+                .bodyToMono(OauthTokenResponse.class)
+                .block();
+    }
+
+    private MultiValueMap<String, String> tokenRequest(String code, ClientRegistration provider) {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("code", code);
+        formData.add("grant_type", "authorization_code");
+        formData.add("redirect_uri", provider.getRedirectUri());
+        formData.add("client_secret", provider.getClientSecret());
+        formData.add("client_id", provider.getClientId());
+        return formData;
+    }
+
+    private User getUserProfile(String providerName, OauthTokenResponse tokenResponse, ClientRegistration provider) {
+        Map<String, Object> userAttributes = getUserAttributes(provider, tokenResponse);
+        Oauth2UserInfo oauth2UserInfo = null;
+        if (providerName.equals("kakao")) {
+            oauth2UserInfo = new KakaoUserInfo(userAttributes);
+        } else {
+            log.info("허용되지 않은 접근입니다.");
+        }
+
+        String provide = oauth2UserInfo.getProvider();
+        String providerId = oauth2UserInfo.getProviderId();
+        String nickName = oauth2UserInfo.getNickName();
+        String email = oauth2UserInfo.getEmail();
+        Gender gender = oauth2UserInfo.getGender();
+        String imageUrl = oauth2UserInfo.getImageUrl();
+
+        User userEntity = userRepository.findByEmail(email);
+
+        if (userEntity == null) {
+            userEntity = User.createUser(email, nickName, gender, provider, providerId, imageUrl);
+            userRepository.save(userEntity);
+        }
+        return userEntity;
     }
 }
