fc91315a6b9a4a279145c7b84ab7c720bfb6b4d7
백승은 <tmdms12@gmail.com>
2023-09-21 16:04:19 +0900

    fix: url

--- frontend/src/components/Login/LoginCallBackPage.tsx
+++ frontend/src/components/Login/LoginCallBackPage.tsx
@@ -14,7 +14,7 @@ const LoginCallBackPage = () => {
 		const kakaoLogin = async () => {
 			await axios({
 				method: 'POST',
-				url: `http://j9d203.p.ssafy.io/api/web/auth?code=${code}`,
+				url: `https://j9d203.p.ssafy.io/api/web/auth?code=${code}`,
 				headers: {
 					'Content-Type': 'application/json;charset=utf-8', //json형태로 데이터를 보내겠다는뜻
 				},
