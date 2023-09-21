225c4bfa27034d1fec0409454cdcac7b30adfd62
백승은 <tmdms12@gmail.com>
2023-09-21 15:04:10 +0900

    feat: 로그인 페이지 추가
    - 로그인 페이지 추가
    - 로그인 콜백 페이지 추가

--- frontend/src/routes/RootRouter.tsx
+++ frontend/src/routes/RootRouter.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
+import LoginPage from '../component/Login/LoginPage';
+import LoginCallBackPage from '../component/Login/LoginCallBackPage';
 
 const RootRouter = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Home />}></Route>
+				<Route path="/login" element={<LoginPage />}></Route>
+				<Route path="/oauth2/redirect" element={<LoginCallBackPage />}></Route>
 			</Routes>
 		</BrowserRouter>
 	);
