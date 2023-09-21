ac5180ad67835491a854e6e300c8514ed61533d7
백승은 <tmdms12@gmail.com>
2023-09-21 15:43:00 +0900

    feat: 백 연결 추가

--- frontend/src/components/Login/LoginPage.tsx
+++ frontend/src/components/Login/LoginPage.tsx
@@ -4,16 +4,6 @@ import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
 	const navigation = useNavigate();
-	// const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-	// const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
-	// console.log('id:', CLIENT_ID);
-	// // oauth 요청 URL
-	// const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-
-	// const onClickLoginButton = useCallback(() => {
-	// 	window.location.href = kakaoURL;
-	// 	console.log(kakaoURL);
-	// }, []);
 
 	useEffect(() => {
 		if (localStorage.getItem('login')) {
@@ -28,9 +18,6 @@ const LoginPage = () => {
 				a태그 로그인
 			</a>
 			<br />
-			{/* <button className="bg-green-500" onClick={onClickLoginButton}>
-				<img src="" alt="로그인" />
-			</button> */}
 		</div>
 	);
 };
