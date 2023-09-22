d04d01543ccf5992e5e4f91b17f32d381af01d20
심은진 <seg412@naver.com>
2023-09-22 15:24:17 +0900

    Feature/be/reviews

---
+++ web/src/main/java/com/gechu/web/review/service/ReviewService.java
@@ -0,0 +1,11 @@
+package com.gechu.web.review.service;
+
+import com.gechu.web.review.dto.ReviewDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ReviewService {
+    public void insertReview(ReviewDto reviewDto);
+
+    public void updateReview(ReviewDto reviewDto);
+}
