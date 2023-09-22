4d1753f9def76088e194654eaea375a0f7af29f3
이찬희 <ych0716@naver.com>
2023-09-22 17:22:58 +0900

    feat: requestmapping 수정

--- game/src/main/java/com/gechu/game/game/controller/GameController.java
+++ game/src/main/java/com/gechu/game/game/controller/GameController.java
@@ -17,7 +17,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/games")
+@RequestMapping(value = "/games")
 public class GameController {
 
 	@GetMapping
