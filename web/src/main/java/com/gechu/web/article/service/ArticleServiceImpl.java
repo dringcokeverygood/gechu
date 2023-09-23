9d2ba28d7d5827ab9465b330b243bb55d6566e1a
신경희 <skh1615@gmail.com>
2023-09-23 22:12:18 +0900

    Feat: ArticleService 작성중

---
+++ web/src/main/java/com/gechu/web/article/service/ArticleServiceImpl.java
@@ -0,0 +1,10 @@
+package com.gechu.web.article.service;
+
+import com.gechu.web.article.dto.ArticleDto;
+
+public class ArticleServiceImpl implements ArticleService{
+    @Override
+    public ArticleDto findArticle(Long articleSeq) {
+        return null;
+    }
+}
