0c2150865100301c93b5cb9b9bc3fa268a4d74dc
신경희 <skh1615@gmail.com>
2023-09-24 16:08:24 +0900

    Feat: GameArticle api테스트중-useState props전달문제

--- frontend/src/components/Games/containers/GameArticleContainer.tsx
+++ frontend/src/components/Games/containers/GameArticleContainer.tsx
@@ -1,6 +1,14 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+import { http } from '../../../utils/http';
 import { GameArticleType } from '../../../typedef/Game/games.types';
 import GameArticle from '../GameArticle';
+
+interface GetArticle {
+	article: GameArticleType;
+	success: boolean;
+	message: string;
+}
 
 const GameArticleContainer = () => {
 	const [imgModalFlag, setImgModalFlag] = useState(false);
@@ -8,20 +16,41 @@ const GameArticleContainer = () => {
 		setImgModalFlag(!imgModalFlag);
 	}, [imgModalFlag]);
 
-	//추후 seq기반으로 fetch해오기
-	const article: GameArticleType = {
+	const articleSeq = useParams().seq;
+	// console.log(articleSeq);
+	const [article, setArticle] = useState<GameArticleType>({
 		seq: 1,
 		gameSeq: 1,
-		gameTitle: '갓게임',
+		gameTitle: 'dummy',
 		userSeq: 1,
-		userNickname: '재밌으면 우는 애옹이',
-		articleTitle: '재밌다옹',
+		userNickname: 'dummy',
+		articleTitle: 'dummy',
 		content:
 			'애오옹애옹애오옹애오옹애옹애오옹애오옹애옹애오옹애오옹애옹애오옹야옹야옹애오옹야옹야옹애오옹야옹야옹애오옹야옹야옹애옹야옹애옹야옹애옹야옹애옹',
 		imageUrl:
 			'https://images.unsplash.com/photo-1612287230202-1ff1d85d1bdf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80',
 		createDate: '2023-09-14',
-	};
+	});
+	useEffect(() => {
+		http.get<GetArticle>(`web/articles/${articleSeq}`).then((data) => {
+			setArticle(data.article);
+		});
+	}, []);
+
+	//더미
+	// const article: GameArticleType = {
+	// 	seq: 1,
+	// 	gameSeq: 1,
+	// 	gameTitle: '갓게임',
+	// 	userSeq: 1,
+	// 	userNickname: '재밌으면 우는 애옹이',
+	// 	articleTitle: '재밌다옹',
+	// 	content:
+	// 		'애오옹애옹애오옹애오옹애옹애오옹애오옹애옹애오옹애오옹애옹애오옹야옹야옹애오옹야옹야옹애오옹야옹야옹애오옹야옹야옹애옹야옹애옹야옹애옹야옹애옹',
+	// 	imageUrl:
+	// 		'https://images.unsplash.com/photo-1612287230202-1ff1d85d1bdf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80',
+	// 	createDate: '2023-09-14',
+	// };
 
 	return (
 		<GameArticle
