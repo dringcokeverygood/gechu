1354b3899434e8fc23e91a28a3286e59d68307f8
신경희 <skh1615@gmail.com>
2023-09-24 16:39:03 +0900

    Feat: crossorigin localhost:3000 테스트

--- web/src/main/java/com/gechu/web/article/controller/ArticleController.java
+++ web/src/main/java/com/gechu/web/article/controller/ArticleController.java
@@ -13,6 +13,7 @@ import java.util.*;
 @RestController
 @RequestMapping("/articles")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class ArticleController {
 
     private final ArticleService articleService;
