8a1fe7a94aaab71e60a563252da9665db4858022
백승은 <tmdms12@gmail.com>
2023-09-24 11:54:12 +0900

    fix: 콜백페이지 롤백

--- frontend/src/components/Login/LoginCallBackPage.tsx
+++ frontend/src/components/Login/LoginCallBackPage.tsx
@@ -1,36 +1,36 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import axios from 'axios';
 
-// 인가코드 백으로 보내는 작업 하는 곳
-
 const LoginCallBackPage = () => {
+	const navigate = useNavigate();
 	const code = new URL(window.location.href).searchParams.get('code');
-	// console.log('code: ', code);
+	console.log('code: ', code);
+	//인가코드 백으로 보내는 코드
 
-	// 요청을 보낼 URL 및 데이터
-	const url = 'https://kauth.kakao.com/oauth/token';
-	const data = {
-		grant_type: 'authorization_code',
-		client_id: process.env.REACT_APP_KAKAO_CLIENT_ID,
-		redirect_uri: 'https://j9d203.p.ssafy.io/login/oauth2/code/kakao',
-		code: code,
-	};
+	useEffect(() => {
+		const kakaoLogin = async () => {
+			await axios({
+				method: 'POST',
+				url: `https://j9d203.p.ssafy.io/api/web/auth?code=${code}`,
+				headers: {
+					'Content-Type': 'application/json;charset=utf-8', //json형태로 데이터를 보내겠다는뜻
+				},
+			})
+				.then((res) => {
+					//백에서 완료후 우리사이트 전용 토큰 넘겨주는게 성공했다면
+					console.log(res);
+					//계속 쓸 정보들( ex: 이름) 등은 localStorage에 저장
+					localStorage.setItem('name', res.data.account.kakaoName);
+					navigate('/');
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		};
 
-	// Axios를 사용하여 HTTP POST 요청 보내기
-	axios
-		.post(url, null, {
-			params: data,
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-			},
-		})
-		.then((response) => {
-			console.log('응답 데이터:', response.data);
-			// 요청이 성공하면 응답 데이터를 출력합니다.
-		})
-		.catch((error) => {
-			console.error('오류:', error);
-			// 요청이 실패하면 오류를 출력합니다.
-		});
+		kakaoLogin();
+	});
 
 	return (
 		<div className="bg-white-100">
