d07eb7bafff84b5e22d6851a8a70d34c9799af5a
백승은 <tmdms12@gmail.com>
2023-09-25 15:09:26 +0900

    feat: 리뷰 모달 수정

--- frontend/src/components/Games/ReviewModal.tsx
+++ frontend/src/components/Games/ReviewModal.tsx
@@ -5,9 +5,15 @@ type Props = {
 	onChangeModalFlag: () => void;
 	onClickPref: (e: React.MouseEvent) => void;
 	preference: { like: boolean; unlike: boolean };
+	isButtonActive: boolean;
 };
 
-const ReviewModal = ({ onChangeModalFlag, onClickPref, preference }: Props) => {
+const ReviewModal = ({
+	onChangeModalFlag,
+	onClickPref,
+	preference,
+	isButtonActive,
+}: Props) => {
 	return (
 		<div className="fixed left-0 top-0 z-50 flex h-full w-full  text-white-950">
 			<div className="h-full w-full bg-white-950 bg-opacity-50 "></div>
@@ -15,28 +21,28 @@ const ReviewModal = ({ onChangeModalFlag, onClickPref, preference }: Props) => {
 			{/* 모달창 */}
 			<div className="fixed left-1/2 top-1/2 flex h-[500px] w-[800px] -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-8 rounded-xl bg-white-100 px-16 pb-16 pt-16">
 				<p className="text-start font-dungGeunMo text-4xl">리뷰 등록</p>
-				<div className="flex justify-center gap-3">
+				<div className="flex justify-center gap-3 text-lg">
 					<p
 						id="like"
 						onClick={onClickPref}
 						className={
 							preference.like
-								? `flex cursor-pointer items-center gap-1 rounded-full border-2 border-solid border-blue-500 bg-blue-500 px-2 py-1 font-dungGeunMo text-[12px] text-white-100`
-								: `flex cursor-pointer items-center gap-1 rounded-full border-2 border-solid border-blue-500 px-2 py-1 font-dungGeunMo text-[12px] text-blue-500`
+								? `flex cursor-pointer items-center gap-1 rounded-full border-2 border-solid border-blue-500 bg-blue-500 px-2 py-1 font-dungGeunMo  text-white-100`
+								: `flex cursor-pointer items-center gap-1 rounded-full border-2 border-solid border-blue-500 px-2 py-1 font-dungGeunMo  text-blue-500`
 						}
 					>
-						좋아요 <MdThumbUpOffAlt size={16} id="like" />
+						좋아요 <MdThumbUpOffAlt size={24} id="like" />
 					</p>
 					<p
 						id="unlike"
 						onClick={onClickPref}
 						className={
 							preference.unlike
-								? `flex cursor-pointer items-center gap-1 rounded-full border-2 border-solid border-red-400 bg-red-400 px-2 py-1 font-dungGeunMo text-[12px] text-white-100`
-								: `flex cursor-pointer items-center gap-1 rounded-full border-2 border-solid border-red-400 px-2 py-1 font-dungGeunMo text-[12px] text-red-400`
+								? `flex cursor-pointer items-center gap-1 rounded-full border-2 border-solid border-red-400 bg-red-400 px-2 py-1 font-dungGeunMo  text-white-100`
+								: `flex cursor-pointer items-center gap-1 rounded-full border-2 border-solid border-red-400 px-2 py-1 font-dungGeunMo  text-red-400`
 						}
 					>
-						싫어요 <MdThumbDownOffAlt size={16} id="unlike" />
+						싫어요 <MdThumbDownOffAlt size={24} id="unlike" />
 					</p>
 				</div>
 				<textarea
@@ -53,8 +59,11 @@ const ReviewModal = ({ onChangeModalFlag, onClickPref, preference }: Props) => {
 						취소
 					</button>
 					<button
-						className="mx-2 h-10 w-16 rounded-xl bg-blue-400 hover:bg-blue-700"
+						className={`mx-2 h-10 w-16 rounded-xl bg-blue-400${
+							isButtonActive ? ' hover:bg-blue-700' : ' pointer-events-none'
+						}`}
 						onClick={onChangeModalFlag}
+						disabled={!isButtonActive}
 					>
 						등록
 					</button>
