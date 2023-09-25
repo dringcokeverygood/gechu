3d1e2f6ef01c328174dd8b0dbb8f81bed374028d
신경희 <skh1615@gmail.com>
2023-09-25 23:08:38 +0900

    Feat: articleRepository 추가

--- web/src/main/java/com/gechu/web/article/service/ArticleServiceImpl.java
+++ web/src/main/java/com/gechu/web/article/service/ArticleServiceImpl.java
@@ -1,10 +1,17 @@
 package com.gechu.web.article.service;
 
 import com.gechu.web.article.dto.ArticleDto;
+import com.gechu.web.article.entity.ArticleEntity;
+import com.gechu.web.article.repository.ArticleRepository;
+import com.gechu.web.review.entity.ReviewEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ArticleServiceImpl implements ArticleService{
+
+    private final ArticleRepository articleRepository;
     @Override
     public ArticleDto findArticle(Long articleSeq) {
         return null;
@@ -12,6 +19,6 @@ public class ArticleServiceImpl implements ArticleService{
 
     @Override
     public void insertArticle(ArticleDto articleDto) {
-
+        articleRepository.save(ArticleEntity.builder().build());
     }
 }
