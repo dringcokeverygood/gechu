0d0f51a2ca36b16134f7f66b3deeeaf407257a3a
심은진 <seg412@naver.com>
2023-09-25 15:12:26 +0900

    fix: comflict 해결

--- web/src/main/java/com/gechu/web/game/service/GameService.java
+++ web/src/main/java/com/gechu/web/game/service/GameService.java
@@ -2,10 +2,6 @@ package com.gechu.web.game.service;
 
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 525ef2aa33c30288e325f1399f5d38145bf72463
 import com.gechu.web.game.dto.GameDetailDto;
 import org.springframework.stereotype.Service;
 
