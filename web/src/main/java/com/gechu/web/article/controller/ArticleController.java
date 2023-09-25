b5aed158acc3630b43cacdcc811f8f1dd241c4b6
신경희 <skh1615@gmail.com>
2023-09-25 15:11:35 +0900

    Feat: articlecontroller 작성중

--- web/src/main/java/com/gechu/web/article/controller/ArticleController.java
+++ web/src/main/java/com/gechu/web/article/controller/ArticleController.java
@@ -1,13 +1,17 @@
 package com.gechu.web.article.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gechu.web.article.dto.ArticleDto;
 import com.gechu.web.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -37,14 +41,21 @@ public class ArticleController {
         return new ResponseEntity<>(resultMap, status);
     }
     @PostMapping("")
-    public ResponseEntity<?> insertArticle(@RequestBody ArticleDto articleDto, HttpServletRequest request){
+    public ResponseEntity<?> insertArticle(
+                @RequestPart("article") String article,
+                @RequestPart(value = "file", required = false) MultipartFile file,
+                HttpServletRequest request) throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ArticleDto articleDto = objectMapper.readValue(article, ArticleDto.class);
         Map<String, Object> resultMap = new HashMap<>();
         HttpStatus status;
 
         try {
+            //토큰 추출
 
             resultMap.put("success", true);
-            status = HttpStatus.OK;
+            status = HttpStatus.CREATED;
         }  catch (Exception e) {
             e.printStackTrace();
             resultMap.put("success", false);
