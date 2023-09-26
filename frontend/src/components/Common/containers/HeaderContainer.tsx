b62ac8a52d29ec6ac5a6704e8be238c020632706
백승은 <tmdms12@gmail.com>
2023-09-26 14:11:58 +0900

    feat: 리코일 로그인 관리 추가

--- frontend/src/components/Common/containers/HeaderContainer.tsx
+++ frontend/src/components/Common/containers/HeaderContainer.tsx
@@ -1,12 +1,15 @@
 import React, { useState, useCallback, useRef } from 'react';
 import Header from '../Header';
 import { useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { LoginAtom } from '../../../recoil/LoginAtom';
 
 const HeaderContainer = () => {
 	const searchWordRef = useRef('');
 	const [searchWord, setSearchWord] = useState('');
 	const isLogin = true;
 	const navigate = useNavigate();
+	const [isLoggedIn, setIsLoggedIn] = useRecoilState(LoginAtom);
 
 	const onChangeSearchWord = useCallback(
 		(
@@ -46,7 +49,8 @@ const HeaderContainer = () => {
 
 	const onClickLogout = () => {
 		localStorage.removeItem('token');
-		localStorage.removeItem('login');
+		setIsLoggedIn(false);
+		console.log(isLoggedIn);
 		console.log('로그아웃');
 	};
 
