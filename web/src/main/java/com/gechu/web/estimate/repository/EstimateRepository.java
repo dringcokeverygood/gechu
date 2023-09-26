dd9ea0cd58c184d3f23921b1cd6c848f4816fc88
이찬희 <ych0716@naver.com>
2023-09-26 15:17:39 +0900

    estimate 삽입, 수정, 탐색 코드 추가

--- web/src/main/java/com/gechu/web/estimate/repository/EstimateRepository.java
+++ web/src/main/java/com/gechu/web/estimate/repository/EstimateRepository.java
@@ -4,9 +4,12 @@ import com.gechu.web.estimate.entity.EstimateEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EstimateRepository extends JpaRepository<EstimateEntity, Long> {
-    Optional<EstimateEntity> findBySeq(Long seq);
+    List<EstimateEntity> findByGameSeq(Long seq);
+
+    EstimateEntity findByGameSeqAndUserSeq(Long gameSeq, Long userSeq);
 }
