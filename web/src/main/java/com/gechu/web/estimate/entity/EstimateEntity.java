ee4e71935542286d56eb131ac6c219473ecdb612
이찬희 <ych0716@naver.com>
2023-09-26 15:12:32 +0900

    feat: estimate 업서트, findbygame 코드 추가

--- web/src/main/java/com/gechu/web/estimate/entity/EstimateEntity.java
+++ web/src/main/java/com/gechu/web/estimate/entity/EstimateEntity.java
@@ -3,39 +3,49 @@ package com.gechu.web.estimate.entity;
 import javax.persistence.*;
 
 import com.gechu.web.estimate.dto.EstimateDto;
+import com.gechu.web.review.entity.ReviewEntity;
 import com.gechu.web.user.entity.UsersEntity;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor @AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "estimate")
 @Builder
 public class EstimateEntity {
 
-    @Id
-    @GeneratedValue
-    private Long seq;
-    private Long gameSeq;
-    private String userLike;
+	@Id
+	@GeneratedValue
+	private Long seq;
+	private Long gameSeq;
+	private String userLike;
 
-    @ManyToOne
-    @JoinColumn(name = "user_seq")
-    private UsersEntity users;
+	@ManyToOne
+	@JoinColumn(name = "user_seq")
+	private UsersEntity users;
 
-    public EstimateDto toDto(EstimateEntity estimateEntity) {
-        if(estimateEntity == null) {
-            return null;
-        }
-        return EstimateDto.builder()
-                .seq(estimateEntity.getSeq())
-                .userSeq(estimateEntity.getUsers().getSeq())
-                .gameSeq(estimateEntity.getGameSeq())
-                .like(estimateEntity.getUserLike())
-                .build();
-    }
+	@OneToOne(mappedBy = "review")
+	ReviewEntity reviewEntity;
+
+	public static EstimateDto toDto(EstimateEntity estimateEntity) {
+		if (estimateEntity == null) {
+			return null;
+		}
+		return EstimateDto.builder()
+			.seq(estimateEntity.getSeq())
+			.userSeq(estimateEntity.getUsers().getSeq())
+			.gameSeq(estimateEntity.getGameSeq())
+			.like(estimateEntity.getUserLike())
+			.build();
+	}
+
+	public void updateUserLike(String like) {
+		this.userLike = like;
+	}
 }
 
