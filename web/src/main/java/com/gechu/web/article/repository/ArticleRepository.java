2546af496aa376796a4969798a0026c89911c87a
심은진 <seg412@naver.com>
2023-09-26 09:24:00 +0900

    Feat: 내 게시글 목록 확인

--- web/src/main/java/com/gechu/web/article/repository/ArticleRepository.java
+++ web/src/main/java/com/gechu/web/article/repository/ArticleRepository.java
@@ -1,10 +1,13 @@
 package com.gechu.web.article.repository;
 
 import com.gechu.web.article.entity.ArticleEntity;
+import com.gechu.web.user.entity.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
-
+    List<ArticleEntity> findByUserSeq(Long userSeq);
 }
