47278cb7268858a103aaff205487c85e2c0ebdc5
이찬희 <ych0716@naver.com>
2023-09-26 17:52:04 +0900

    feat: feignclient gameserver rest 요청 완성?

--- web/src/main/java/com/gechu/web/game/service/GameServiceClient.java
+++ web/src/main/java/com/gechu/web/game/service/GameServiceClient.java
@@ -4,12 +4,26 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.gechu.web.game.dto.GameResponseDto;
 
-@FeignClient(name = "game")
+@FeignClient(name = "game", url = "https://j9d203.p.ssafy.io/api/game")
 public interface GameServiceClient {
 
-	@GetMapping("/game/games")
+	@GetMapping("/games")
 	public List<GameResponseDto> findGames();
+
+	@PostMapping("/games/list/seq")
+	public List<GameResponseDto> findGamesBySeqs(List<Integer> seqs);
+
+	@PostMapping("/games/list/seq")
+	public List<GameResponseDto> findGamesBySlugs(List<String> slugs);
+
+	@GetMapping("/games/slug/{gameSlug}")
+	public GameResponseDto findGameBySlug(@PathVariable("gameSlug") String gameSlug);
+
+	@GetMapping("/games/seq/{gameSeq}")
+	public GameResponseDto findGameBySeq(@PathVariable("gameSeq") Integer gameSeq);
 }
