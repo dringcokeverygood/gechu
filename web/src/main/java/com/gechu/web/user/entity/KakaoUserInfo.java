42535608703d5436dfc2041a05e2f5f4120efbd1
장성운 <sungoon0646@gmail.com>
2023-10-02 22:31:03 +0900

    feat : 로그인할 때 userSeq 넘겨주기

--- web/src/main/java/com/gechu/web/user/entity/KakaoUserInfo.java
+++ web/src/main/java/com/gechu/web/user/entity/KakaoUserInfo.java
@@ -3,14 +3,25 @@ package com.gechu.web.user.entity;
 import lombok.AllArgsConstructor;
 import com.gechu.web.user.entity.Gender;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Map;
 
-
-@AllArgsConstructor
 @Getter
 public class KakaoUserInfo implements Oauth2UserInfo {
     private Map<String, Object> attributes;
+
+    @Setter
+    private Long seq;
+
+    public KakaoUserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public KakaoUserInfo(Map<String, Object> attributes, Long seq) {
+        this.attributes = attributes;
+        this.seq = seq;
+    }
 
     @Override
     public String getProviderId() {
