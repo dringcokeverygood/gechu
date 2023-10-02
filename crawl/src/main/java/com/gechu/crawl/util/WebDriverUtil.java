8649e01f107a5be46d9862bf1d7303dd66bf82f3
ych0716@naver.com <ych0716@naver.com>
2023-10-02 23:08:12 +0900

    feat: path 설정을 위한 삽질

--- crawl/src/main/java/com/gechu/crawl/util/WebDriverUtil.java
+++ crawl/src/main/java/com/gechu/crawl/util/WebDriverUtil.java
@@ -31,56 +31,56 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class WebDriverUtil {
 
-	private WebDriver driver;
-	@Value("${spring.driver.path}")
-	private String driverPath;
-	private static final String url = "https://www.metacritic.com/game/";
+	// private WebDriver driver;
+	// @Value("${spring.driver.path}")
+	// private String driverPath;
+	// private static final String url = "https://www.metacritic.com/game/";
 	private final CrawlMetaCriticAsync crawlMetaCriticAsync;
 
 	// chromedriver 경로 체크용 함수 (더이상 안씀)
-	public void checkDirectory() throws IOException {
-		Path directoryPath = Paths.get("");
-
-		log.info("디렉토리 트리 내의 모든 파일과 디렉토리를 나열 (하위 디렉토리 포함)");
-		Files.walkFileTree(directoryPath, EnumSet.noneOf(FileVisitOption.class), Integer.MAX_VALUE,
-			new SimpleFileVisitor<Path>() {
-				@Override
-				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-					if (file.endsWith("chromedriver") || file.endsWith("chromedriver.exe")) {
-						log.info("{}", file);
-					}
-					return FileVisitResult.CONTINUE;
-				}
-
-				@Override
-				public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-					// 파일 접근 오류 처리
-					System.err.println(exc);
-					return FileVisitResult.CONTINUE;
-				}
-			});
-	}
-
-	public void chrome() {
-		Path currentPath = Paths.get("");
-		String path = currentPath.toAbsolutePath().toString();
-		log.info("현재 작업 경로: " + path);
-		log.info("driverPath  =" + driverPath);
-
-		System.setProperty("webdriver.chrome.driver", driverPath);
-
-		ChromeOptions options = new ChromeOptions();
-		options.setHeadless(true);
-		options.addArguments("--lang=ko");
-		options.addArguments("--no-sandbox");
-		options.addArguments("--disable-dev-shm-usage");
-		options.addArguments("--disable-gpu");
-		options.setCapability("ignoreProtectedModeSettings", true);
-		options.addArguments("--remote-allow-origins=*");
-
-		this.driver = new ChromeDriver(options);
-		driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
-	}
+	// public void checkDirectory() throws IOException {
+	// 	Path directoryPath = Paths.get("");
+	//
+	// 	log.info("디렉토리 트리 내의 모든 파일과 디렉토리를 나열 (하위 디렉토리 포함)");
+	// 	Files.walkFileTree(directoryPath, EnumSet.noneOf(FileVisitOption.class), Integer.MAX_VALUE,
+	// 		new SimpleFileVisitor<Path>() {
+	// 			@Override
+	// 			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+	// 				if (file.endsWith("chromedriver") || file.endsWith("chromedriver.exe")) {
+	// 					log.info("{}", file);
+	// 				}
+	// 				return FileVisitResult.CONTINUE;
+	// 			}
+	//
+	// 			@Override
+	// 			public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+	// 				// 파일 접근 오류 처리
+	// 				System.err.println(exc);
+	// 				return FileVisitResult.CONTINUE;
+	// 			}
+	// 		});
+	// }
+	//
+	// public void chrome() {
+	// 	Path currentPath = Paths.get("");
+	// 	String path = currentPath.toAbsolutePath().toString();
+	// 	log.info("현재 작업 경로: " + path);
+	// 	log.info("driverPath  =" + driverPath);
+	//
+	// 	System.setProperty("webdriver.chrome.driver", driverPath);
+	//
+	// 	ChromeOptions options = new ChromeOptions();
+	// 	options.setHeadless(true);
+	// 	options.addArguments("--lang=ko");
+	// 	options.addArguments("--no-sandbox");
+	// 	options.addArguments("--disable-dev-shm-usage");
+	// 	options.addArguments("--disable-gpu");
+	// 	options.setCapability("ignoreProtectedModeSettings", true);
+	// 	options.addArguments("--remote-allow-origins=*");
+	//
+	// 	this.driver = new ChromeDriver(options);
+	// 	driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
+	// }
 
 	public void multiThreading(List<String> gameSlugs) {
 		for (String gameSlug : gameSlugs) {
@@ -88,33 +88,33 @@ public class WebDriverUtil {
 		}
 	}
 
-	public void crawlMetaCriticUserReviews(String gameSlug) {
-		// try {
-		driver.get(url + gameSlug + "/user-reviews/");
-		driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
-
-		WebElement element = null;
-		// element = driver.findElement(By.xpath(
-		// 	"/html/body/div[1]/div/div/div[2]/div[1]/div[1]/section/div[6]/div[1]/div/div[1]/div[2]/div/span"));
-		//
-		// log.info("---------------------------------------------------------------------------------------------------------");
-		//
-		// log.info("element: {}", element);
-		// log.info("element.toString: {}", element.toString());
-		//
-		// log.info("log: {}", element.getText());
-		// log.info("---------------------------------------------------------------------------------------------------------");
-
-		String pageSource = driver.getPageSource();
-
-		Document document = Jsoup.parse(pageSource);
-
-		String html = document.body().html();
-		log.info("html: {}", html);
-	}
-
-	private void quit() {
-		driver.close();
-		driver.quit();
-	}
+	// public void crawlMetaCriticUserReviews(String gameSlug) {
+	// 	// try {
+	// 	driver.get(url + gameSlug + "/user-reviews/");
+	// 	driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+	//
+	// 	WebElement element = null;
+	// 	// element = driver.findElement(By.xpath(
+	// 	// 	"/html/body/div[1]/div/div/div[2]/div[1]/div[1]/section/div[6]/div[1]/div/div[1]/div[2]/div/span"));
+	// 	//
+	// 	// log.info("---------------------------------------------------------------------------------------------------------");
+	// 	//
+	// 	// log.info("element: {}", element);
+	// 	// log.info("element.toString: {}", element.toString());
+	// 	//
+	// 	// log.info("log: {}", element.getText());
+	// 	// log.info("---------------------------------------------------------------------------------------------------------");
+	//
+	// 	String pageSource = driver.getPageSource();
+	//
+	// 	Document document = Jsoup.parse(pageSource);
+	//
+	// 	String html = document.body().html();
+	// 	log.info("html: {}", html);
+	// }
+	//
+	// private void quit() {
+	// 	driver.close();
+	// 	driver.quit();
+	// }
 }
