c034b51f2608542ab16934743359ab89959218f6
ych0716@naver.com <ych0716@naver.com>
2023-10-03 00:44:48 +0900

    feat: 일단 점수부터 넣어보자

--- crawl/src/main/java/com/gechu/crawl/config/AsyncConfig.java
+++ crawl/src/main/java/com/gechu/crawl/config/AsyncConfig.java
@@ -14,8 +14,8 @@ public class AsyncConfig extends AsyncConfigurerSupport {
 	@Override
 	public Executor getAsyncExecutor() {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(1);
-		executor.setMaxPoolSize(20);
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(10);
 		executor.setQueueCapacity(50);
 		executor.setThreadNamePrefix("meta-async-");
 		executor.initialize();
