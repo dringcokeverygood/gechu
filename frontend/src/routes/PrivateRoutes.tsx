0f777321cf11b5f541a4772734409a4f651d91f7
백승은 <tmdms12@gmail.com>
2023-10-04 22:55:21 +0900

    fix: 라우터 로그인 정보 수정

--- frontend/src/routes/PrivateRoutes.tsx
+++ frontend/src/routes/PrivateRoutes.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { LoginAtom } from '../recoil/LoginAtom';
 
 type Props = {
 	redirectTo: string;
 };
 
 const PrivateRoutes = ({ redirectTo }: Props) => {
-	const isLogin = localStorage.getItem('login') ? true : false;
+	const isLogin = useRecoilValue(LoginAtom);
 
 	return isLogin ? <Outlet /> : <Navigate to={redirectTo} replace />;
 };
