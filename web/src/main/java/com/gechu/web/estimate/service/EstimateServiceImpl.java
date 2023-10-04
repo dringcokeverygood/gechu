3c261973ab082bdcc61481547d0eb7e3d4101d6d
심은진 <seg412@naver.com>
2023-10-04 13:10:53 +0900

    Feat: review 로그 추가

--- web/src/main/java/com/gechu/web/estimate/service/EstimateServiceImpl.java
+++ web/src/main/java/com/gechu/web/estimate/service/EstimateServiceImpl.java
@@ -20,45 +20,51 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class EstimateServiceImpl implements EstimateService {
 
-	private final EstimateRepository estimateRepository;
+    private final EstimateRepository estimateRepository;
 
-	@Override
-	@Transactional
-	public Long upsertEstimate(EstimateDto estimateDto) {
-		EstimateEntity estimateEntity = estimateRepository.findByGameSeqAndUsers_Seq(estimateDto.getGameSeq(),
-			estimateDto.getUserSeq());
+    @Override
+    @Transactional
+    public Long upsertEstimate(EstimateDto estimateDto) {
+        EstimateEntity estimateEntity = estimateRepository.findByGameSeqAndUsers_Seq(estimateDto.getGameSeq(),
+                estimateDto.getUserSeq());
 
-		if (estimateEntity != null) {
-			estimateEntity.updateUserLike(estimateDto.getLike());
-			return estimateEntity.getSeq();
-		}
+        if (estimateEntity != null) {
+            estimateEntity.updateUserLike(estimateDto.getLike());
+            return estimateEntity.getSeq();
+        }
 
-		EstimateEntity save = estimateRepository.save(EstimateDto.toEntity(estimateDto));
-		return save.getSeq();
-	}
+        EstimateEntity save = estimateRepository.save(EstimateDto.toEntity(estimateDto));
+        return save.getSeq();
+    }
 
-	@Override
-	public List<EstimateDto> findEstimatesByGameSeq(Long gameSeq) {
-		List<EstimateEntity> estimateEntities = estimateRepository.findByGameSeq(gameSeq);
-		log.info("service -> {}", estimateEntities.get(0).getUserLike());
-		log.info("service users -> {}", estimateEntities.get(0).getUsers().getSeq());
-		if (estimateEntities.size() == 0) {
-			return new ArrayList<>();
-		}
-		return estimateEntities.stream().map(EstimateEntity::toDto).collect(Collectors.toList());
-	}
+    @Override
+    public List<EstimateDto> findEstimatesByGameSeq(Long gameSeq) {
+        log.info("service의 최상단");
+        List<EstimateEntity> estimateEntities = null;
+        try {
+            estimateEntities = estimateRepository.findByGameSeq(gameSeq);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        log.info("service -> {}", estimateEntities.get(0).getUserLike());
+        log.info("service users -> {}", estimateEntities.get(0).getUsers().getSeq());
+        if (estimateEntities.size() == 0) {
+            return new ArrayList<>();
+        }
+        return estimateEntities.stream().map(EstimateEntity::toDto).collect(Collectors.toList());
+    }
 
-	public List<EstimateDto> findEstimatesWhereReviewIsNotNullByUserSeq(Long userSeq) {
-		List<EstimateEntity> estimateEntities = estimateRepository.findByUsers_Seq(userSeq);
-		return estimateEntities.stream()
-			.filter(e -> e.getReviewEntity() != null)
-			.map(EstimateEntity::toDto)
-			.collect(Collectors.toList());
-	}
+    public List<EstimateDto> findEstimatesWhereReviewIsNotNullByUserSeq(Long userSeq) {
+        List<EstimateEntity> estimateEntities = estimateRepository.findByUsers_Seq(userSeq);
+        return estimateEntities.stream()
+                .filter(e -> e.getReviewEntity() != null)
+                .map(EstimateEntity::toDto)
+                .collect(Collectors.toList());
+    }
 
-	public List<EstimateDto> findEstimatesByUserSeq(Long userSeq) {
-		List<EstimateEntity> estimateEntities = estimateRepository.findByUsers_Seq(userSeq);
-		return estimateEntities.stream().map(EstimateEntity::toDto).collect(Collectors.toList());
-	}
-	
+    public List<EstimateDto> findEstimatesByUserSeq(Long userSeq) {
+        List<EstimateEntity> estimateEntities = estimateRepository.findByUsers_Seq(userSeq);
+        return estimateEntities.stream().map(EstimateEntity::toDto).collect(Collectors.toList());
+    }
+
 }
