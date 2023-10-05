b30c8bafe335644b71750b0fb0c39bb88a07a24c
이찬희 <ych0716@naver.com>
2023-10-05 14:40:21 +0900

    feat: 댓글 삭제 url 변경

--- web/src/main/java/com/gechu/web/comment/controller/CommentController.java
+++ web/src/main/java/com/gechu/web/comment/controller/CommentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,12 +62,12 @@ public class CommentController {
 		return new ResponseEntity<>(resultMap, status);
 	}
 
-	@DeleteMapping
-	public ResponseEntity<?> deleteComment(@RequestBody Map<String, Object> map) {
+	@DeleteMapping("/{commentSeq}")
+	public ResponseEntity<?> deleteComment(@PathVariable("commentSeq") Long commentSeq) {
 		Map<String, Object> resultMap = new HashMap<>();
 		HttpStatus status;
 		try {
-			Long commentSeq = commentService.deleteComment((Long)map.get("commentSeq"));
+			commentService.deleteComment(commentSeq);
 			resultMap.put("commentSeq", commentSeq);
 			resultMap.put("success", true);
 			status = HttpStatus.OK;
