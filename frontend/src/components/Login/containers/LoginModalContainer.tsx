bcb4a19875a64ea4dcaa6dee78637b1522004c7c
백승은 <tmdms12@gmail.com>
2023-10-05 10:22:48 +0900

    feat: 로그인 후 페이지 이동 수정

--- frontend/src/components/Login/containers/LoginModalContainer.tsx
+++ frontend/src/components/Login/containers/LoginModalContainer.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import LoginModal from '../LoginModal';
+import { useSetRecoilState } from 'recoil';
+import { UrlAtom } from '../../../recoil/UrlAtom';
 
 type Props = {
 	onClickLoginModalBtn: () => void;
 };
 
 const LoginModalContainer = ({ onClickLoginModalBtn }: Props) => {
-	return <LoginModal onClickLoginModalBtn={onClickLoginModalBtn} />;
+	const setToUrl = useSetRecoilState(UrlAtom);
+
+	const onClickSetUrl = () => {
+		const toUrl: string = window.location.pathname;
+		setToUrl(toUrl);
+	};
+
+	return (
+		<LoginModal
+			onClickLoginModalBtn={onClickLoginModalBtn}
+			onClickSetUrl={onClickSetUrl}
+		/>
+	);
 };
 
 export default LoginModalContainer;
